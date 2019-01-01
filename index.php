ecb5bd7097032fc2bbd66fa5fe3cb69273fa1503
metronidazolesb <44513796+metronidazolesb@users.noreply.github.com>
2019-01-01 17:04:35 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="profile.css">
+		<title>Sam Bradford</title>
+	</head>
+
+
+	<body>
+	
+		<header>
+			<big>Sam Bradford</big>
+			<br>
+			<small>"Fear does not exist anywhere except in the mind". - Dale Carnegie</small>
+		</header>
+
+		<div class="mainpanel">
+				<section>
+					<h2>Who am I?</h2>
+					<p>
+						For those of you who do not know me, my name is Sam. I am an early career health practitioner
+						who grew up in the Wide Bay region. I thoroughly enjoyed my time there often spending
+						too long at the beach playing volleyball and swimming or being towed behind boats up
+						and down the Mary River. As all good things do, my time in the Wide Bay came to an end.
+					</p>
+	
+					<p>
+						After my time in the Wide Bay, I moved to North Queensland to complete my education.
+						In NQ I met many great people with whom I had many adventures around the region.
+						I spent some time in NQ, and during that time I fell in love with it. Alas, it would
+						not last and I left for work.
+					</p>
+	
+					<p>
+						My work has brought me to Central Queensland, closer to home. I have been in CQ for
+						two years now and will likely be here for another four as I complete a Master's program.
+					</p>
+				</section>
+
+				<section>
+					<h2>So now you know</h2>
+	
+					<p>
+						...a little about me. If you would like to know more, or learn about things I want to share,
+						then please follow this site! I hope to make a blog, perhaps a podcast if I have time and 
+						some educational posts.
+					</p>
+				</section>
+
+				<section>
+					<h2>For those of you who KNOW me</h2>
+					<p>
+						Please see below for my contact details if you wish to reconnect.
+						I always enjoy catching up with old friends.
+						<br><br>
+						Regards,<br><br>Sam
+					</p>
+				</section>
+			</div>
+
+			<div class="sidepanel">
+				<h2>Site Map</h2>
+				<ul>
+					<li>
+						Content:
+						<ul>
+							<li>Blog</li>
+						</ul>
+					</li>
+					<li>
+						Personal:
+						<ul>
+							<li>Email</li>
+						</ul>
+					</li>
+					<li>
+						Support:
+						<ul>
+							<li>Bitcoin</li>
+						</ul>
+					</li>
+				</ul>
+			</div>	
+			
+	</body>
+	
+</html>
